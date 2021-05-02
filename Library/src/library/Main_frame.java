ed23d2b7f1c57c8dee78941988d40b8b698492c6
Ashrakat <witch7997@gmail.com>
2021-05-02 06:16:25 +0200

    Main frame

--- Library/src/library/Main_frame.java
+++ Library/src/library/Main_frame.java
@@ -38,6 +38,11 @@ public class Main_frame extends javax.swing.JFrame {
         jLabel1.setText("Public Library System");
 
         jButton1.setText("Manage books");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Manage users");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -88,6 +93,12 @@ public class Main_frame extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        book_management b = new book_management();
+        b.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
